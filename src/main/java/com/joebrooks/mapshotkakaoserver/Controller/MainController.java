767a5628982a3805ae47b7f59583f9a86c381167
lcw3176 <lcw3176@naver.com>
2021-08-13 00:44:02 +0900

    크로스 오리진 설정

--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
@@ -4,11 +4,12 @@ import com.joebrooks.mapshotkakaoserver.Service.EXDriverService;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"https://mapshot.netlify.app, https://testservermapshot.netlify.app"})
+@CrossOrigin("https://mapshot.netlify.app")
 @RestController
 @RequestMapping("/main")
 public class MainController {
@@ -31,11 +32,12 @@ public class MainController {
         sb.append(lng);
         sb.append("&level=");
         sb.append(level);
-
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "https://mapshot.netlify.app");
         driverService.getDriver().get(sb.toString());
         driverService.getWaiter().until(ExpectedConditions.presenceOfElementLocated(By.id("checker-true")));
         byte[] srcFile = driverService.getDriver().getFullScreenshotAs(OutputType.BYTES);
 
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(srcFile);
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.IMAGE_JPEG).body(srcFile);
     }
 }
--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/TestController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/TestController.java
@@ -1,15 +1,43 @@
 package com.joebrooks.mapshotkakaoserver.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.joebrooks.mapshotkakaoserver.Service.EXDriverService;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("https://testservermapshot.netlify.app")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    private EXDriverService driverService;
+
+    public TestController(EXDriverService driverService){
+        this.driverService = driverService;
+    }
+
     @GetMapping
-    public String hello(){
-        return "hello";
+    public ResponseEntity getMapCapture(@RequestParam("lat") String lat,
+                                        @RequestParam("lng") String lng,
+                                        @RequestParam("level") String level) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("https://mapshotproxyserver.herokuapp.com/map");
+        sb.append("?lat=");
+        sb.append(lat);
+        sb.append("&lng=");
+        sb.append(lng);
+        sb.append("&level=");
+        sb.append(level);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "https://testservermapshot.netlify.app");
+        driverService.getDriver().get(sb.toString());
+        driverService.getWaiter().until(ExpectedConditions.presenceOfElementLocated(By.id("checker-true")));
+        byte[] srcFile = driverService.getDriver().getFullScreenshotAs(OutputType.BYTES);
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.IMAGE_JPEG).body(srcFile);
     }
 }
