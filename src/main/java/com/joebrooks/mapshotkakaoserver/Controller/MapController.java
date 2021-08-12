24303db6b44a9ce9beaccba1c2cff8ad93ecb315
lcw3176 <lcw3176@naver.com>
2021-08-13 00:30:18 +0900

    크로스 오리진 설정

--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MapController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MapController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "https://mapshot.netlify.app, https://testservermapshot.netlify.app")
+@CrossOrigin(origins = "https://mapshotproxyserver.herokuapp.com")
 @Controller
 @RequestMapping("/map")
 public class MapController {
