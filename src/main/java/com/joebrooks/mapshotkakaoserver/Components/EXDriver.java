021166954a3e826bcb42cd7274ae77ed688833d4
lcw3176 <lcw3176@naver.com>
2021-08-12 17:07:59 +0900

    경로 추가

--- src/main/java/com/joebrooks/mapshotkakaoserver/Components/EXDriver.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Components/EXDriver.java
@@ -14,12 +14,13 @@ public class EXDriver {
 
     @PostConstruct
     public void initialize() throws Exception {
-        System.setProperty("webdriver.chrome.driver","D:/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
+        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
 
         driver = new ChromeDrvierEX(options);
     }
