368c7632dfbc04d5ccd040b1cb54dba576a3d8f2
joebrooks <lcw3176@naver.com>
2021-08-13 17:45:22 +0900

    헤더 추가

--- src/main/java/com/joebrooks/mapshotkakaoserver/Filter/CORSFilter.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Filter/CORSFilter.java
@@ -16,9 +16,11 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "https://testservermapshot.netlify.app");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-        chain.doFilter(req, res);
+        response.setHeader("Access-Control-Allow-Headers", "Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If");
+        response.setHeader("Access-Control-Expose-Headers",  "DAV, content-length, Allow");
+
+        chain.doFilter(request, response);
     }
 }
