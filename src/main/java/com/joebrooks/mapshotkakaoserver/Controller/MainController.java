949f7a8848def7cd9e080595b3993fbd3a2439a9
joebrooks <lcw3176@naver.com>
2021-08-13 17:33:10 +0900

    cors 어노테이션 제거

--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("https://mapshot.netlify.app")
 @RestController
 @RequestMapping("/main")
 public class MainController {
--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/TestController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("https://testservermapshot.netlify.app")
 @RestController
 @RequestMapping("/test")
 public class TestController {
