30f2bdd06a9d3a47ccfa3fe157a86aa6d3ab821b
joebrooks <lcw3176@naver.com>
2021-08-15 10:49:58 +0900

    버그 픽스

--- src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Controller/MainController.java
@@ -1,9 +1,12 @@
 package com.joebrooks.mapshotkakaoserver.Controller;
 
 import com.joebrooks.mapshotkakaoserver.Service.EXDriverService;
+import com.joebrooks.mapshotkakaoserver.Utils.ChromeDrvierEX;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +26,7 @@ public class MainController {
     public ResponseEntity getMapCapture(@RequestParam("lat") String lat,
                                         @RequestParam("lng") String lng,
                                         @RequestParam("level") String level,
-                                        @RequestParam("type") String type) {
+                                        @RequestParam("type") String type) throws Exception {
 
         StringBuilder sb = new StringBuilder();
         sb.append("https://mapshotproxyserver.herokuapp.com/map");
@@ -36,9 +39,22 @@ public class MainController {
         sb.append("&type=");
         sb.append(type);
 
-        driverService.getDriver().get(sb.toString());
-        driverService.getWaiter().until(ExpectedConditions.presenceOfElementLocated(By.id("checker-true")));
-        byte[] srcFile = driverService.getDriver().getFullScreenshotAs(OutputType.BYTES);
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
+
+        ChromeDrvierEX driver = new ChromeDrvierEX(options);
+        WebDriverWait waiter = new WebDriverWait(driver, 30);
+
+        driver.get(sb.toString());
+        waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("checker-true")));
+        byte[] srcFile = driver.getFullScreenshotAs(OutputType.BYTES);
+
+        driver.quit();
 
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(srcFile);
     }
