5023fc79e50c24cf2aa62f6c0c46e8d371957899
joebrooks <lcw3176@naver.com>
2021-08-15 13:09:04 +0900

    서비스 수정

--- src/main/java/com/joebrooks/mapshotkakaoserver/Services/EXDriverService.java
+++ src/main/java/com/joebrooks/mapshotkakaoserver/Services/EXDriverService.java
@@ -1,35 +1,40 @@
 package com.joebrooks.mapshotkakaoserver.Services;
 
 
-import com.joebrooks.mapshotkakaoserver.Components.EXDriver;
-import com.joebrooks.mapshotkakaoserver.Components.EXWaiter;
 import com.joebrooks.mapshotkakaoserver.Utils.ChromeDrvierEX;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PreDestroy;
 
 @Service
 public class EXDriverService{
-    private EXDriver drivers;
-    private EXWaiter waiter;
 
-    public EXDriverService(EXDriver drivers, EXWaiter waiter){
-        this.drivers = drivers;
-        this.waiter = waiter;
+
+//    public WebDriverWait getWaiter() {
+//        return waiter.getWaiter();
+//    }
+
+    @Async("driverExecutor")
+    public ChromeDrvierEX getDriver() throws Exception {
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
+
+        ChromeDrvierEX driver = new ChromeDrvierEX(options);
+
+        return driver;
     }
 
-    public WebDriverWait getWaiter() {
-        return waiter.getWaiter();
-    }
-
-    public ChromeDrvierEX getDriver() {
-        return drivers.getDriver();
-    }
-
-    @PreDestroy
-    public void close() {
-        drivers.getDriver().quit();
-    }
+//    @PreDestroy
+//    public void close() {
+//        drivers.getDriver().quit();
+//    }
 
 }
